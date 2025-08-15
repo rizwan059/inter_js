fa59900233c0e2e0a2ca41b9179e5fa2737ff3cc
Kabir-Sial <sial059689@gmail.com>
2025-08-15 23:30:04 +0500

    cracked the querry selector and querrySelectorAll

--- intermediat js basics/inter.js
+++ intermediat js basics/inter.js
@@ -1,5 +1,16 @@
-let lis = document.querySelectorAll("li");
-for (let i = 0; i < lis.length; i++){
+// let lis = document.querySelectorAll("li");
+// for (let i = 0; i < lis.length; i++){
 
-    console.log(lis[i].textContent);
-}
+//     console.log(lis[i].textContent);
+// }
+
+// add a title to a div via js 
+
+// let a = document.querySelector("div");
+// a.setAttribute("title", "lorem ipsum")
+
+//how to change the font size via querry selector
+let font = document.querySelectorAll("p");
+font.forEach(function(elem){
+    elem.style.fontSize = "18px";
+})
