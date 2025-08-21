65591b57449208efcb04b4c0e65911648888c17f
Kabir-Sial <sial059689@gmail.com>
2025-08-21 23:21:46 +0500

    cracked the js event listener functions concept

--- input_card/form.js
+++ input_card/form.js
@@ -3,4 +3,33 @@ let inputs = document.querySelectorAll("input");
 
 form.addEventListener("submit", function(dets){
     dets.preventDefault();
+
+let card = document.createElement("div");
+card.classList.add("card");
+
+let profile = document.createElement("div");
+profile.classList.add("profile");
+
+let img = document.createElement("img");
+img.setAttribute = "src","https://images.unsplash.com/photo-1755429562521-cb944ea054ab?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE5fHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D"
+
+let h3 = document.createElement("h3");
+h3.textContent = "Rizwan Ali";
+
+let h5 = document.createElement("h5");
+h5.textContent = "Struggling Developer";
+
+let para = document.createElement("p");
+para.textContent = "sadfsadfsadfsafsafdsdfsadfsadfsaf";
+
+
+profile.appendChild(img);
+card.appendChild(profile);
+
+card.appendChild(h3);
+card.appendChild(h5);
+card.appendChild(para);
+
+
+form.appendChild(card); 
 });
