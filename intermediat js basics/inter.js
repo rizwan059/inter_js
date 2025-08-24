67da30c600e053ece04ac7d0f4f75fe9ed24ab30
Kabir-Sial <sial059689@gmail.com>
2025-08-24 23:15:56 +0500

    adding the folder

--- intermediat js basics/inter.js
+++
@@ -1,9 +0,0 @@
-let sel  = document.querySelector("select");
-let selectionDevice = document.querySelector("#selDev");
-
-sel.addEventListener("change", function(dets){
-   
-    selectionDevice.textContent = `${dets.target.value} Device Selected`;
-    // console.log(dets);
-
-})
