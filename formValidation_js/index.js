c4099b1ad9dde46a0492a2946866a1f871886865
Kabir-Sial <sial059689@gmail.com>
2025-08-30 00:30:02 +0500

    validator is now working fine as js is ok now

--- formValidation_js/index.js
+++ formValidation_js/index.js
@@ -6,16 +6,29 @@ let form = document.querySelector("form");
 form.addEventListener("submit", function (dets) {
     dets.preventDefault();
 
-    // document.querySelector("emailans").textContent = "";
+    
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    
+    document.querySelector("#emailError").textContent = "";
+    let emailans = emailRegex.test(email.value);
+    let passwordans = passwordRegex.test(password.value);
 
-    const emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
-    const passwordRegex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$";
+    if (!emailans) {
+        document.querySelector("#emailError").textContent = "Invalid email";
+    } else {
+        document.querySelector("#emailError").textContent = "Everything is fine";
+        document.querySelector("#emailError").style.color = "Green";
 
-     let emailans = emailRegex.test(email.value);
-    // let passwordans = passwordRegex.test(password.value);
+    }
 
-if (!emailans){
-document.querySelector("#emailError").textContent = "Invalid data";
-}
+
+    if (!passwordans) {
+        document.querySelector("#passwordError").textContent = "Invalid password";
+    } else {
+        document.querySelector("#passwordError").textContent = "Everything is fine";
+        document.querySelector("#passwordError").style.color = "Green";
+
+    }
 
 });
