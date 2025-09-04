67418180db792c5ddcd3152f2ac1bea771258ba5
Kabir-Sial <sial059689@gmail.com>
2025-09-04 05:37:13 +0500

    local storage concept throught window preference theme

---
+++ LDTheme/LTTheme.js
@@ -0,0 +1,32 @@
+function lightDark() {
+
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.body.classList.add("dark")
+        document.body.classList.remove("light")
+    } else {
+        document.body.classList.add("light")
+        document.body.classList.remove("dark")
+
+    }
+}
+lightDark();
+
+
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", function(){
+    if (document.body.classList.contains("dark")){
+        document.body.classList.remove("dark");
+        document.body.classList.add("light");
+    } else {
+         document.body.classList.remove("light");
+        document.body.classList.add("dark");
+    }
+})
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener("change", function(){
+
+    lightDark();
+})
+
+
